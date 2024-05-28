9b46d833595e84cea9b7ee42056b4118e1d0e330
Eric Lluis <ericlluis84@gmail.com>
2024-05-28 18:01:17 +0200

    Add component App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+const App = (): React.ReactElement => {
+  return (
+    <div className="main-container">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
