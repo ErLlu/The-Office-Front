86dd01baec7fe4a3345edace3ebbd6525dea0083
Eric Lluis <ericlluis84@gmail.com>
2024-06-03 20:39:38 +0200

    Add test workflow

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 describe("Given the Header component", () => {
   describe("When rendered", () => {
     test("Then it should show 'The Office App' inside a heading", () => {
-      const expectedTitle = /the office app/;
+      const expectedTitle = /the office app/i;
 
       render(<Header />);
 
