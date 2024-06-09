0de082935a125ab8ba3040f13caeea501fe46e90
Eric Lluis <ericlluis84@gmail.com>
2024-06-09 15:05:54 +0200

    Test Home page component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,19 +2,21 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import CharactersClient from "../characters/client/CharactersClient";
 import { loadCharactersActionCreator } from "../characters/slice/charactersSlice";
-import CharactersList from "../characters/components/CharactersList/CharactersList";
+import CharactersList from "../characters/components/CharactersList/CharactersList.js";
 
 const HomePage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
   const { characters } = useAppSelector((state) => state.characters);
 
   useEffect(() => {
-    async () => {
+    const fetchCharacters = async () => {
       const client = new CharactersClient();
       const characters = await client.getAll();
       const action = loadCharactersActionCreator(characters);
       dispatch(action);
     };
+
+    fetchCharacters();
   }, [dispatch]);
 
   return <CharactersList characters={characters} />;
--- src/router/mainRouter.tsx
+++ src/router/mainRouter.tsx
@@ -5,11 +5,13 @@ import {
   Route,
 } from "react-router-dom";
 import App from "../characters/components/App/App";
+import HomePage from "../pages/HomePage";
 
 const mainRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route element={<Navigate to="/characters" />} index />
+      <Route path="/characters" element={<HomePage />} />
     </Route>,
   ),
 );
