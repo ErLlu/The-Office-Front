8b0ff90c674ae93c815d8abaad25a943beb84cf9
Eric Lluis <ericlluis84@gmail.com>
2024-06-09 15:04:59 +0200

    Create a Home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import CharactersClient from "../characters/client/CharactersClient";
+import { loadCharactersActionCreator } from "../characters/slice/charactersSlice";
+import CharactersList from "../characters/components/CharactersList/CharactersList";
+
+const HomePage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+  const { characters } = useAppSelector((state) => state.characters);
+
+  useEffect(() => {
+    async () => {
+      const client = new CharactersClient();
+      const characters = await client.getAll();
+      const action = loadCharactersActionCreator(characters);
+      dispatch(action);
+    };
+  }, [dispatch]);
+
+  return <CharactersList characters={characters} />;
+};
+
+export default HomePage;
