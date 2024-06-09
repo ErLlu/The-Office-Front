b25a316b680573aeece7e4d75ff3e9e1fa6b8a32
Eric Lluis <ericlluis84@gmail.com>
2024-06-09 14:40:30 +0200

    Create CharacterCard component

---
+++ src/characters/components/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,33 @@
+import { Character } from "../../types";
+
+interface CharacterCardProps {
+  character: Character;
+}
+
+const CharacterCard = ({
+  character,
+}: CharacterCardProps): React.ReactElement => {
+  return (
+    <article className="character">
+      <h2 className="character__name">{`${character.name}`}</h2>
+      <div className="character__position">{character.position}</div>
+      <div className="character__image">
+        <img
+          className="character__image"
+          src={character.imageUrl}
+          alt={character.alternativeText}
+          width="150"
+          height="150"
+        />
+      </div>
+      <div className="character__age">{character.age}</div>
+      <div className="character__seasons">{character.seasons}</div>
+      <div className="character__status">
+        {character.isWorking ? "Está trabajando" : "No está trabajando"}
+      </div>
+      <p className="character__description">{character.description}</p>
+    </article>
+  );
+};
+
+export default CharacterCard;
