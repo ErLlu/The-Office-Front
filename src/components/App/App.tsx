f07410d48187a8e0fcbe45c158d7b243d9de8c01
Eric Lluis <ericlluis84@gmail.com>
2024-06-14 19:36:46 +0200

    Refactor styles image header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,25 @@
 import { Outlet } from "react-router-dom";
-import "./App.scss";
 import Header from "../Header/Header";
 import NavigationMenu from "../NavigationMenu/NavigationMenu";
+import "./App.scss";
 
 const App = () => {
   return (
-    <div className="main-container">
-      <Header />
-      <NavigationMenu />
-      <main className="main-content">
-        <Outlet />
-      </main>
+    <div>
+      <img
+        className="header__image"
+        src="https://distritooficina.com/wp-content/uploads/2023/08/image-1080x757.jpg"
+        alt="Imagen de la oficina de The Office"
+        width="415"
+        height="78"
+      />
+      <div className="main-container">
+        <Header />
+        <NavigationMenu />
+        <main className="main-content">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,15 +3,6 @@ import "./Header.scss";
 const Header = (): React.ReactElement => {
   return (
     <header className="main-header">
-      <div>
-        <img
-          className="main-header__image"
-          src="https://distritooficina.com/wp-content/uploads/2023/08/image-1080x757.jpg"
-          alt="Imagen de la oficina de The Office"
-          width="415"
-          height="78"
-        />
-      </div>
       <h1 className="main-title">The Office App</h1>
     </header>
   );
